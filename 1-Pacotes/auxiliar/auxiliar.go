9d3581f7d23e25e70ff60dd4015f3925a4165a2f
Lucas Barbosa <soares_lucas13@hotmail.com>
2023-02-03 08:50:43 -0300

    pacotes

--- 1-Pacotes/auxiliar/auxiliar.go
+++ 1-Pacotes/auxiliar/auxiliar.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func Escrever() {
 	fmt.Println("Escrevendo do pacote auxiliar")
+	escrever2()
 }
--- 1-Pacotes/main.go
+++ 1-Pacotes/main.go
@@ -8,4 +8,5 @@ import (
 func main() {
 	fmt.Println("Olá Mundo")
 	auxiliar.Escrever()
+
 }
