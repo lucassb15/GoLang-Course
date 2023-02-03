d6a730865b263ffa486dfcac85d0f93ad5cd2739
Lucas Barbosa <soares_lucas13@hotmail.com>
2023-02-03 14:24:55 -0300

    operadores

---
+++ 5-Operadores/operadores.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Aritméticos
+	soma := 1 + 2
+	subtracao := 1 - 2
+	divisao := 10 / 4
+	multiplicacao := 10 * 5
+	restoDaDivisao := 10 % 2
+
+	fmt.Println(soma, subtracao, divisao, multiplicacao, restoDaDivisao)
+
+	var numero1 int16 = 10
+	var numero2 int32 = 25
+	somas := numero1 + int16(numero2)
+	fmt.Println(somas)
+}
